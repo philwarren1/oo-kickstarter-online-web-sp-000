b0193b848582c67295d95c577ea59252a2761e78
Philip Warren <philwarren1@gmail.com>
2020-08-12 18:32:22 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  
+  attr_reader :name, :backed_projects, :project
+  
+   
+   def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @project = project
+    @backed_projects << project
+    project.backers << self
+ end
+end 
